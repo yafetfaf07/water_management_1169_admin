528dfd1b364307ad1c7ed318345976ed0d04ac4e
Yafet Yosef <japhetkumalo23@gmail.com>
2024-11-05 14:59:18 +0300

    Aded the functionality of bill creation

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import { DollarSign } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { useEffect, useRef, useState } from "react";
 import { supabase } from "./createClient";
+import { error, log } from "console";
 
 function App() {
   interface UserModel {
@@ -38,6 +39,11 @@ function App() {
     uid: string;
     user_exchange: string;
   }
+  interface CreateBill {
+    price:number;
+    bill_name:string;
+
+  }
   interface SpecificUser {
     transaction_no: string;
     user_name: string;
@@ -49,6 +55,10 @@ function App() {
     user_exchange: string;
   }
   const dialogReference = useRef<HTMLDialogElement>(null);
+  const [createbill,setcreatebill] = useState<CreateBill>({
+    bill_name:"",
+    price:0
+  })
   const [suser, setsuser] = useState<SpecificUser>({
     fname: "",
     house_no: "",
@@ -79,9 +89,18 @@ function App() {
     },
   ]);
 
-  const fetchUserById = async (id: string) => {
-    dialogReference.current?.showModal();
+  const createBill = async () => {
+    console.log("Aloha: ");
+    
+    const billCreate = await supabase.from('bill').insert(createbill);
+    if(billCreate.error)
+      console.log("Error: ", billCreate.error);
+    if(billCreate.status=200) {
+      alert("Success");
+    }
+  }
 
+  const fetchUserById = async (id: string) => {
     const userData = await supabase
       .from("user")
       .select("fname,lname,house_no,user_name,phone_no,floor_no")
@@ -91,6 +110,9 @@ function App() {
       .from("bill_reference")
       .select("transaction_no,user_exchange")
       .eq("uid", id);
+    if (billData) {
+      dialogReference.current?.showModal();
+    }
     console.log("Specific user: ", userData.data);
     if (userData.data && billData.data) {
       const { fname, lname, house_no, phone_no, user_name, floor_no } =
@@ -106,20 +128,19 @@ function App() {
         floor_no,
         user_exchange,
       });
-    } else if (userData.data && billData.data==undefined) {
+    } else if (userData.data && billData.data == undefined) {
       const { fname, lname, house_no, phone_no, user_name, floor_no } =
         userData.data[0];
-        setsuser({
-          transaction_no:'',
-          fname,
-          lname,
-          house_no,
-          phone_no,
-          user_name,
-          floor_no,
-          user_exchange:'',
-        });
-
+      setsuser({
+        transaction_no: "",
+        fname:"",
+        lname:"",
+        house_no:"",
+        phone_no:"",
+        user_name:"",
+        floor_no,
+        user_exchange: "",
+      });
     }
     console.log("Specifice bill for a specific user: ", billData.data);
   };
@@ -137,9 +158,9 @@ function App() {
         setbd(billData.data);
         console.log("Bill Data: ", billData.data);
 
-        for (let i = 0; i < billData.data.length; i++) {
-          // if(billData.data[i])
-        }
+        // for (let i = 0; i < billData.data.length; i++) {
+        //   // if(billData.data[i])
+        // }
       }
     };
 
@@ -178,6 +199,10 @@ function App() {
                         type="text"
                         alt=""
                         className="border border-gray-400 w-72 rounded-sm mt-2"
+                        onChange={(e) =>setcreatebill((prevState) => ({
+                          ...prevState,
+                          bill_name:e.target.value
+                        })) }
                       />
                     </div>{" "}
                     <div
@@ -191,10 +216,17 @@ function App() {
                         type="text"
                         alt=""
                         className="border border-gray-400 mt-2 w-72 rounded-sm"
+                        onChange={(e) => setcreatebill((prevState) => ({
+                          ...prevState,
+                          price:Number(e.target.value)
+                        }))}
                       />
                     </div>{" "}
                   </div>
-                  <Button className="p-1 w-24 mt-10 ml-5 ">Create</Button>
+                  <button className="p-1 w-24 mt-10 ml-5 " onClick={() => {
+                    createBill();
+                    
+                  }}>Create</button>
                 </DialogDescription>
               </DialogHeader>
             </DialogContent>
